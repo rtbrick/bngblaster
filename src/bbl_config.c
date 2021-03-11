a4facd6dbe93cee4e5c0936f0a1ea34577d6bedd
Christian Giese <christian@rtbrick.com>
2021-03-11 11:38:46 +0100

    Minor config parser bug

--- src/bbl_config.c
+++ src/bbl_config.c
@@ -746,7 +746,7 @@ json_parse_config (json_t *root, bbl_ctx_s *ctx) {
                 l2tp_server->data_offset = json_boolean_value(value);
             }
         }   
-    } else if (json_is_object(sub)) {
+    } else if (json_is_object(section)) {
         fprintf(stderr, "JSON config error: List expected in L2TP server configuration but dictionary found\n");
     }
 
