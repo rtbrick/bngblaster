37899ccb0d258abb301336b0a54454c96838686a
Christian Giese <christian@rtbrick.com>
2022-05-19 09:58:15 +0000

    fix PPPoE sessions hanging in terminate state

--- code/bngblaster/src/bbl_rx.c
+++ code/bngblaster/src/bbl_rx.c
@@ -1286,6 +1286,12 @@ bbl_rx_lcp(bbl_ethernet_header_t *eth, bbl_interface_s *interface, bbl_session_s
     pppoes = (bbl_pppoe_session_t*)eth->next;
     lcp = (bbl_lcp_t*)pppoes->next;
 
+    if(session->session_state == BBL_PPP_TERMINATING && 
+       !(lcp->code == PPP_CODE_TERM_REQUEST || lcp->code == PPP_CODE_TERM_ACK)) {
+        /* Only term-request/ack is accepted in terminating phase */
+        return;
+    }
+
     switch(lcp->code) {
         case PPP_CODE_VENDOR_SPECIFIC:
             if(ctx->config.lcp_vendor_ignore) {
