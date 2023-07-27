6195b1125294f6400240ca8ea79083b05ddf697b
Hannes Gredler <hannes@rtbrick.com>
2023-07-27 15:19:06 +0000

    add missing include file

---
+++ code/lspgen/src/lspgen_ospf.h
@@ -0,0 +1,32 @@
+/*
+ * Generic Link State Packet generation for link-state protocols.
+ *
+ * OSPF protocol definitions.
+ *
+ * Hannes Gredler, July 2023
+ *
+ * Copyright (C) 2015-2023, RtBrick, Inc.
+ */
+
+#define OSPF_MSG_LSUPDATE          4
+
+#define OSPF_LSA_ROUTER            1
+#define OSPF_LSA_NETWORK           2
+#define OSPF_LSA_SUMMARY           3
+#define OSPF_LSA_ASBR_SUMMARY      4
+#define OSPF_LSA_EXTERNAL          5
+
+#define OSPF_LSA_OPAQUE_LINK       9
+#define OSPF_LSA_OPAQUE_AREA      10
+#define OSPF_LSA_OPAQUE_DOMAIN    11
+
+#define OSPF_TLV_HOSTNAME          7
+
+enum {
+    /* Router LSA */
+    OSPF_ROUTER_LSA_LINK_PTP,
+    OSPF_ROUTER_LSA_LINK_STUB,
+
+    /* External LSA */
+    OSPF_EXTERNAL_PREFIX,
+};
