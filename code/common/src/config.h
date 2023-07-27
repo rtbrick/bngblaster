57d88950c37d3c1f2d00be9ae5abd9e975bbab86
Christian Giese <christian@rtbrick.com>
2023-07-27 17:38:06 +0000

    fix config.h

---
+++ code/common/src/config.h
@@ -0,0 +1,18 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/* CMake Project Details */
+#define PROJECT_NAME "bngblaster"
+#define PROJECT_VER  "0.0.0"
+#define PROJECT_VER_MAJOR "0"
+#define PROJECT_VER_MINOR "0"
+#define PROJECT_VER_PATCH "0"
+
+#define BNGBLASTER_VERSION ""
+#define GIT_REF ""
+#define GIT_SHA ""
+
+#define COMPILER_ID ""
+#define COMPILER_VERSION ""
+
+#endif
