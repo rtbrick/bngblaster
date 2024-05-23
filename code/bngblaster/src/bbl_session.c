01123fd49aecf8300226e39e56dd2da260a07f77
Christian Giese <christian@rtbrick.com>
2024-05-23 07:22:05 +0000

    Revert "remove session-id arg from session-traffic-reset"
    This reverts commit 36a8c1163ea87c92d2411a4dfafae22795d966e9.

--- code/bngblaster/src/bbl_session.c
+++ code/bngblaster/src/bbl_session.c
@@ -1809,14 +1809,12 @@ bbl_session_ctrl_traffic_stop(int fd, uint32_t session_id, json_t *arguments __a
 }
 
 int
-bbl_session_ctrl_traffic_reset(int fd, uint32_t session_id __attribute__((unused)), json_t *arguments __attribute__((unused)))
+bbl_session_ctrl_traffic_reset(int fd, uint32_t session_id, json_t *arguments __attribute__((unused)))
 {
     bbl_session_s *session;
     uint32_t i;
-
-    /* Iterate over all sessions */
-    for(i = 0; i < g_ctx->sessions; i++) {
-        session = &g_ctx->session_list[i];
+    if(session_id) {
+        session = bbl_session_get(session_id);
         if(session) {
             if(g_ctx->stats.session_traffic_flows_verified >= session->session_traffic.flows_verified) {
                 g_ctx->stats.session_traffic_flows_verified -= session->session_traffic.flows_verified;
@@ -1828,9 +1826,27 @@ bbl_session_ctrl_traffic_reset(int fd, uint32_t session_id __attribute__((unused
             bbl_stream_reset(session->session_traffic.ipv6_down);
             bbl_stream_reset(session->session_traffic.ipv6pd_up);
             bbl_stream_reset(session->session_traffic.ipv6pd_down);
+        } else {
+            return bbl_ctrl_status(fd, "warning", 404, "session not found");
         }
-    }   
-
+    } else {
+        /* Iterate over all sessions */
+        for(i = 0; i < g_ctx->sessions; i++) {
+            session = &g_ctx->session_list[i];
+            if(session) {
+                if(g_ctx->stats.session_traffic_flows_verified >= session->session_traffic.flows_verified) {
+                    g_ctx->stats.session_traffic_flows_verified -= session->session_traffic.flows_verified;
+                }
+                session->session_traffic.flows_verified = 0;
+                bbl_stream_reset(session->session_traffic.ipv4_up);
+                bbl_stream_reset(session->session_traffic.ipv4_down);
+                bbl_stream_reset(session->session_traffic.ipv6_up);
+                bbl_stream_reset(session->session_traffic.ipv6_down);
+                bbl_stream_reset(session->session_traffic.ipv6pd_up);
+                bbl_stream_reset(session->session_traffic.ipv6pd_down);
+            }
+        }   
+    }
     return bbl_ctrl_status(fd, "ok", 200, NULL);
 }
 
