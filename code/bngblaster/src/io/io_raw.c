91f44d9cfe40cad9958913b099e84fe38b6e7df0
Christian Giese <christian@rtbrick.com>
2024-07-30 14:25:23 +0000

    reset stream cursor on TX error

--- code/bngblaster/src/io/io_raw.c
+++ code/bngblaster/src/io/io_raw.c
@@ -175,6 +175,7 @@ io_raw_tx_job(timer_s *timer)
                 } else {
                     LOG(IO, "RAW sendto on interface %s failed with error %s (%d)\n", 
                         interface->name, strerror(errno), errno);
+                    io->bucket_cur->stream_cur = stream;
                     io->stats.io_errors++;
                     burst = 0;
                 }
@@ -280,6 +281,7 @@ io_raw_thread_tx_run_fn(io_thread_s *thread)
                     } else {
                         LOG(IO, "RAW sendto on interface %s failed with error %s (%d)\n", 
                             io->interface->name, strerror(errno), errno);
+                        io->bucket_cur->stream_cur = stream;
                         io->stats.io_errors++;
                         burst = 0;
                     }
