000fe06419cb5d384cba7d3a4fda88d63cf42859
Olivier Matz <olivier.matz@6wind.com>
2026-03-20 16:24:24 +0100

    LAC: start PPPoL2TP sessions
    Wire ACCESS_TYPE_PPPOL2TP sessions into the L2TP tunnel lifecycle.
    
    In ctrl job, call bbl_l2tp_client_session_get_tunnel() and enqueue the
    session via bbl_l2tp_client_session_connect(). Sessions that arrive
    before the tunnel reaches ESTABLISHED are queued on a new
    pending_session_qhead in the tunnel; they are drained and connected once
    bbl_l2tp_sccrp_rx() completes the SCCCN handshake.
    
    Add bbl_l2tp_client_session_get_tunnel(): given a PPPoL2TP session, find
    a LAC tunnel associated with the session's configured l2tp-client group
    id.
    
    Make bbl_l2tp_client_session_connect() public: it now takes a
    bbl_session_s * to associate with the new L2TP session.
    
    Set first_session_tx when SCCRQ is sent from bbl_l2tp_client_connect(),
    so that the global setup_time metric is correctly calculated for
    PPPoL2TP sessions (analogous to PADI for PPPoE).
    
    Signed-off-by: Olivier Matz <olivier.matz@6wind.com>

--- code/bngblaster/src/bbl_l2tp.h
+++ code/bngblaster/src/bbl_l2tp.h
@@ -165,6 +165,7 @@ typedef struct bbl_l2tp_tunnel_
     CIRCLEQ_ENTRY(bbl_l2tp_tunnel_) tunnel_qnode;
 
     CIRCLEQ_HEAD(session_, bbl_l2tp_session_) session_qhead;
+    CIRCLEQ_HEAD(pending_session_, bbl_session_) pending_session_qhead;
     CIRCLEQ_HEAD(txq_, bbl_l2tp_queue_) tx_qhead;
 
     /* Pointer to corresponding network interface */
@@ -371,5 +372,11 @@ bbl_l2tp_ctrl_tunnels(int fd, uint32_t session_id __attribute__((unused)), json_
 
 int
 bbl_l2tp_ctrl_lcp_restart(int fd, uint32_t session_id, json_t *arguments __attribute__((unused)));
+
+bbl_l2tp_tunnel_s *
+bbl_l2tp_client_session_get_tunnel(bbl_session_s *session);
+
+void
+bbl_l2tp_client_session_connect(bbl_l2tp_tunnel_s *l2tp_tunnel, bbl_session_s *session);
 
 #endif
