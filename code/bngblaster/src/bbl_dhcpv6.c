0a2ace93ea35854cd34a8c7d5006d16f028f896a
Christian Giese <christian@rtbrick.com>
2026-05-29 20:20:25 +0000

    DHCPv6 IA error handling (#389) - renew

--- code/bngblaster/src/bbl_dhcpv6.c
+++ code/bngblaster/src/bbl_dhcpv6.c
@@ -173,6 +173,7 @@ bbl_dhcpv6_validate_ia(bbl_session_s *session, bbl_dhcpv6_s *dhcpv6)
     switch (session->dhcpv6_state) {
         case BBL_DHCP_SELECTING:
         case BBL_DHCP_REQUESTING:
+        case BBL_DHCP_RENEWING:
             ia_na = true;
             ia_pd = true;
             break;
